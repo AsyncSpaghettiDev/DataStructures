a0721797222251a0faa04207b355ffeca30cd31e
unknown <jafetmv21@gmail.com>
2020-09-30 18:23:39 -0700

    Metodo recursivo de la serie de Fibonacci

---
+++ RecursividadDirecta/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program{
+    static int [] serie;
+    static void Main(string[] args){
+        // Mojica Vidal Jonathan Jafet
+        // 19211688
+
+        // Se prepara la consola
+        Console.Clear();
+        Console.Title = "Captura de Fibonacci";
+
+        // Captura de posicion
+        Console.Write("Ingresa la posición de Fibonacci deseada: ");
+        int ser = Int16.Parse( Console.ReadLine() );
+
+        // Creacion del vector 
+        serie = new int [ser + 1];
+
+        // Se calcula fibonacci
+        Fibonacci(ser);
+    
+        // Se prepara la consola
+        Console.Clear();
+        Console.Title = "Serie de Fibonacci";
+
+        // Despliegue de resultados
+        foreach(int item in serie)
+            Console.Write(item + "\t");
+
+        // Salida del programa
+        Console.Write("\nPresiona una tecla para salir...");
+        Console.ReadLine();
+    }
+    // Se calcula la serie fibonacci y se almacena su valor en la variable global
+    static int Fibonacci(int fib){
+        if (fib == 0 || fib == 1)
+            return serie[fib] = 1;
+        else
+            return serie[fib] = Fibonacci(fib - 1) + Fibonacci(fib - 2);
+    }
+}
