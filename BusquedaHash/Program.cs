868a611b82a82d037ce89c018c6839d31ec00c6a
unknown <jafetmv21@gmail.com>
2020-12-15 17:55:33 -0800

    Estilizar el codigo

--- BusquedaHash/Program.cs
+++ BusquedaHash/Program.cs
@@ -15,7 +15,7 @@ namespace BusquedaHash {
                 Console.Clear();
                 Console.Title = "Menu de Operaciones Busqueda Hash";
 
-                Console.Write("\t\t[1] Generar matriculas aleatorias\n\t\t[2] Despliegue de las matriculas generadas\n\t\t[3] Asignar direcciones a las matriculas\n\t\t[4] Busqueda por Hash\n\t\t[! Otro] Salir\n\t\tSelecciona una opción: ");
+                Console.Write("\t\t\t\t[1] Generar matriculas aleatorias\n\t\t\t\t[2] Despliegue de las matriculas generadas\n\t\t\t\t[3] Asignar direcciones a las matriculas\n\t\t\t\t[4] Busqueda por Hash\n\t\t\t\t[! Otro] Salir\n\t\t\t\tSelecciona una opción: ");
                 switch (Console.ReadLine()) {
                     case "1":
                         matriculas = new HashB();
@@ -52,15 +52,18 @@ namespace BusquedaHash {
                 desOrden [i] = new Random().Next(100, 2000);
                 inOrden [i] = -1;
             }
-            Console.Write("\t\tMatriculas Generadas Correctamente...");
+            Console.Write("\t\t\t\tMatriculas Generadas Correctamente...");
             Console.ReadKey();
         }
         public void Mostrar() {
             Console.Clear();
             Console.Title = "Mostrando las matriculas en desOrden";
-            Console.WriteLine("\tMatriculas registradas");
-            for (int i = 0; i < inOrden.Length; i++) Console.WriteLine("\t{0}-. {1}", i, desOrden [i]);
-            Console.Write("\t\tDatos Desplegados Correctamente...");
+            Console.WriteLine("\t\t\t\t\t\tMatriculas registradas");
+            for (int i = 0; i < inOrden.Length; i++) {
+                if (i % 4 == 0) Console.WriteLine();
+                Console.Write("\t\t{0:D3}-. {1:D4}", i + 1, desOrden [i]);
+            }
+            Console.Write("\n\n\t\t\t\t\tDatos Desplegados Correctamente...");
             Console.ReadKey();
         }
         public void Direccionar() {
@@ -76,22 +79,40 @@ namespace BusquedaHash {
                 }
                 inOrden [PosI] = desOrden [i];
             }
-            Console.WriteLine("\n\t\t\t\t\tValores Capturados \t\t\t Direcciones asignadas");
-            for (int i = 0; i < inOrden.Length; i++) Console.WriteLine("\t \t\t\t {0}.- {1} \t\t\t\t{0}-. {2}", i, desOrden [i], inOrden [i]);
-            Console.Write("\t\t\t \tDatos Ordenados Correctamente...");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("\n\t\t\t   Valores Capturados ");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("\t\t\t\tDirecciones asignadas");
+
+            for (int i = 0; i < inOrden.Length; i++) {
+                if (i % 2 == 0) Console.WriteLine();
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("\t\t {0:D3}.- {1:D4}", i + 1, desOrden [i]);
+
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.Write(" \t\t{0:D3}-. {1:D4}", i + 1, inOrden [i]);
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("\n\n\t\t\t\t\tDirecciones Asignadas Correctamente...");
             Console.ReadKey();
         }
         public void Buscar() {
+            Console.Clear();
+            Console.Title = "Busqueda Hash";
             int PosI, Conflicto, busqueda;
             int tamano = inOrden.Length - 1;
 
-            Console.WriteLine("Matriculas y su clave de hash");
-            for (int i = 0; i < inOrden.Length; i++) Console.WriteLine("\t\t\t\t {0}-.{1}", i + 1, inOrden [i]);
+            Console.WriteLine("\t\t\t\t\t    Matriculas y su clave de hash");
+            for (int i = 0; i < inOrden.Length; i++) {
+                if (i % 4 == 0) Console.WriteLine();
+                Console.Write("\t\t{0:D3}-. {1:D4}", i + 1, inOrden [i]);
+            }
 
-            do Console.Write("\t\t\t\t\n¿Qué matricula deseas buscar?: "); while (!int.TryParse(Console.ReadLine(), out busqueda));
+            do Console.Write("\n\n\t\t\t\t\t    ¿Qué matricula deseas buscar?: "); while (!int.TryParse(Console.ReadLine(), out busqueda));
             PosI = (busqueda % tamano) + 1;
 
-            if (inOrden [PosI] == busqueda) Console.Write("\t\t\t\tLa matricula {0} está en la posición #{1}", busqueda, PosI + 1);
+            if (inOrden [PosI] == busqueda) Console.Write("\t\t\t\t\t La matricula {0} está en la posición #{1}.", busqueda, PosI + 1);
             else {
                 Conflicto = PosI + 1;
                 while (Conflicto <= tamano && inOrden [Conflicto] != busqueda && inOrden [Conflicto] != 0 && Conflicto != PosI) {
@@ -99,10 +120,10 @@ namespace BusquedaHash {
                     if (Conflicto > tamano) Conflicto = 0;
                 }
                 Console.Write(inOrden [Conflicto] == busqueda ?
-                    String.Format("\t\t\t\t El elemento {0} está en la posición #{1}", busqueda, Conflicto + 1) :
-                    String.Format("\t\t\t\t El elemento {0} no está en el arreglo", busqueda));
-                Console.ReadKey();
+                    String.Format("\t\t\t\t\t La matricula {0} está en la posición #{1}.", busqueda, Conflicto + 1) :
+                    String.Format("\t\t\t\t\t La matricula {0} no está en el arreglo.", busqueda));
             }
+            Console.ReadKey();
         }
     }
 }
